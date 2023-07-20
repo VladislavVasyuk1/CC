eb29dfb17b9d138caa796a5f14112fc588285d17
VLad <hok000hok@gmail.com>
2023-07-20 21:39:44 +0300

    first semenar complict

---
+++ simenar1.1/Program.cs
@@ -0,0 +1,42 @@
+﻿/*int number = 22;
+double doubleNumber = 23.32;
+string myName ='Vlad';
+bool logicVar = true;
+*/
+/*task 1
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("your number is " + num);
+*/
+
+
+/*tASK2 Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quad2 = num2 * num2;
+if (num1 == quad2)
+{   
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+/* task 3 Напишите программу, которая на вход принимает одно положительное число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+while ( current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+// task 4
+
